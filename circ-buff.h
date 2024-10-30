b0649198f54757661f4b6b4360880acb7c38d09d
Bibhas B <bibhas2@gmail.com>
2024-10-30 09:27:54 -0400

    Simplify code

--- circ-buff.h
+++ circ-buff.h
@@ -7,27 +7,30 @@ struct CircBuff {
 	size_t start = 0;
 	size_t capacity = CAPACITY;
 	size_t size = 0;
+    size_t end = 0;
 
     void add(const T& value) {
+        //Set value at the current end position
+        data[end] = value;
+
         if (size < capacity) {
+            //Grow size
             ++size;
-
-            data[size - 1] = value;
         }
-        else {
-            size_t index = start;
 
-            start += 1;
+        //Move end forward
+        end = (end + 1) % capacity;
 
-            start = start % capacity;
-
-            data[index] = value;
+        if (size == capacity) {
+            //At full capacity start and end
+            //must coincide
+            start = end;
         }
     }
 
     T& at(size_t pos) {
         if (pos >= size) {
-            throw std::out_of_range("Given index is outside the size of.");
+            throw std::out_of_range("Index is outside the size of buffer.");
         }
 
         size_t i = start + pos;
